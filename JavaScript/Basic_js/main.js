98a0ddafd38d26aa9edeff38b4b2a1ecdb26a828
Harshit Paneri <harshitpanari@gmail.com>
2022-11-11 11:57:56 +0530

    final day 9

--- JavaScript/Basic_js/main.js
+++ JavaScript/Basic_js/main.js
@@ -146,3 +146,19 @@ function myFunc(total, num) {
   ])
 
   console.log(x);
+  // 
+  function myFunction() {
+    const message = document.getElementById("p01");
+    message.innerHTML = "";
+    let x = document.getElementById("demo").value;
+    try {
+      if(x == "") throw "empty";
+      if(isNaN(x)) throw "not a number";
+      x = Number(x);
+      if(x < 5) throw "too low";
+      if(x > 10) throw "too high";
+    }
+    catch(err) {
+      message.innerHTML = "Input is " + err;
+    }
+  }
