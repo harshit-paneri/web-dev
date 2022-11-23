8832bac18bab13d63e9ae5f1d3327a3837277f58
Harshit Paneri <harshitpanari@gmail.com>
2022-11-24 00:55:40 +0530

    sigin complete

--- my_store/controllers/auth.controller.js
+++ my_store/controllers/auth.controller.js
@@ -1,114 +1,97 @@
 const UserModel = require("../database/models/Users");
 const cryptop = require("crypto");
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 
 exports.signup = async (req, res) => {
   const usData = req.body;
-  try{
-  const userExist =await UserModel.findOne({ email: usData.email }); //findOne or another funtion = exits it return true or false
-  //try and catch implement
-  
-  if (userExist) {
+  try {
+    const userExist = await UserModel.findOne({ email: usData.email }); //findOne or another funtion = exits it return true or false
+    //try and catch implement
+
+    if (userExist) {
+      res.send({
+        statusCode: 200,
+        message: "user already exists.",
+        error: false,
+        data: userExist,
+      });
+    } else {
+      // const salt = "jkvnksdjvnfdlkvd";
+      const password = usData.password + process.env.salt;
+      const hash = cryptop.createHash("sha1");
+      hash.update(password);
+      const hashPassword = hash.digest("hex");
+
+      const newUser = new UserModel({
+        name: usData.name,
+        email: usData.email,
+        mobile_number: usData.mobile_number,
+        password: hashPassword,
+      });
+
+      await newUser.save();
+
+      const token = jwt.sign({ userId: newUser._id }, process.env.SECRET_KEY);
+      //console.log(token);
+      res.send({
+        statusCode: 200,
+        message: "user created sussefully.",
+        error: false,
+        data: newUser,
+        token: token,
+      });
+    }
+  } catch (error) {
     res.send({
-      statusCode: 200,
-      message: "user already exists.",
-      error: false,
-      data: userExist,
+      statusCode: 400,
+      message: error.message,
+      error: true,
+      data: null,
     });
   }
-  else{
-  // const salt = "jkvnksdjvnfdlkvd";
-  const password = usData.password+process.env.salt;
-  const hash = cryptop.createHash("sha1");
-  hash.update(password);
-  const hashPassword = hash.digest("hex");
-
-  const newUser = new UserModel({
-    name : usData.name,
-    email : usData.email,
-    mobile_number : usData.mobile_number,
-    password: hashPassword
-  });
-
-await newUser.save();
-
-const token = jwt.sign({userId : newUser._id},  process.env.SECRET_KEY);
-//console.log(token);
-
-
-  
-res.send({
-  statusCode: 200,
-  message: "user created sussefully.",
-  error: false,
-  data: newUser,
-  token:token,
-});
-} 
-
-}catch(error){
-  res.send({
-    statusCode:400,
-    message:error.message,
-    error:true,
-    data:null,
-  });
-}
-
-//signin
-exports.signin = async (req, res) =>{
-  // try{
-  //   const userData = req.body;
-
-  //   const search =await UserModel.findOne({ email: usData.email },{password:usData.password});
-  //   const check = UserModel({
-  //     email : usData.email,
-  //     password: hashPassword
-  //   });
-
-  //   res.send({
-  //     statusCode: 200,
-  //     message: "sign in sussefully.",
-  //     error: false,
-  //     data: newUser,
-  //     token:token,
-  //   });
-  // }catch (error) {
-  //   res.send({
-  //     statusCode: 400,
-  //     message: error.message,
-  //     error: true,
-  //     data: null,
-  //   });
-  }
-
 };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  //signin
+  exports.signin = async (req, res) => {
+    try {
+      const usData = req.body;
+      const hash = cryptop.createHash("sha1");
+      hash.update(usData.password + process.env.SALT);
+      const Password = hash.digest("hex");
+      const existCheck = await UserModel.findOne({
+        email: usData.email,
+        password: Password,
+      });
+      if (existCheck) {
+        res.send({
+          statusCode: 200,
+          message: "Logged In",
+          error: false,
+          data: existCheck,
+        });
+      } else {
+        res.send({
+          statusCode: 400,
+          message: "User not found. Create a new user",
+          error: true,
+        });
+      }
+    } catch (error) {
+      res.send({
+        statusCode: 400,
+        message: error.message,
+        error: true,
+        data: null,
+      });
+    }
+  };
 
 
 // exports.signin = async (req, res) =>{
 //   try{
 //     const userData = req.body;
 
-//     // const search =await 
+//     // const search =await
 //     UserModel.findOne({ email: usData.email },{password:usData.password});
 //     // const check = UserModel({
 //     //   email : usData.email,
@@ -126,8 +109,7 @@ exports.signin = async (req, res) =>{
 //     }
 //     var token = jwt.sign({ email : usData.email }, usData.password, { //config.secret
 //       expiresIn: 86400 // 24 hours
-//     });    
-
+//     });
 
 //     res.send({
 //       statusCode: 200,
@@ -145,7 +127,6 @@ exports.signin = async (req, res) =>{
 //     });
 //   }
 //  };
-
 
 //     try{
 //     const userAuth = req.body;
--- my_store/routes/auth.route.js
+++ my_store/routes/auth.route.js
@@ -4,6 +4,6 @@ const authController = require("../controllers/auth.controller");
 
 
 authRouter.post("/signup",authController.signup);
-// authRouter.post("/signin",authController.signin);
+authRouter.post("/signin",authController.signin);
 // router.post("/",postAuth.signup);
 module.exports = authRouter;
